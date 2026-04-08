44fcb684266553cad76db432120efaf93bad1011
hermana <aeherman@ualberta.ca>
2026-04-07 21:40:52 -0600

    more keybindings

--- keycrop/src/keyMap.ts
+++ keycrop/src/keyMap.ts
@@ -4,7 +4,7 @@ export const KEY_MAP: { key: string; category: string; capital_key: string; comm
   { key: 'ctrl+shift+\\', category: 'Navigating Code', capital_key: "CTRL+SHIFT+\\", command: 'jump_to_bracket', description: "Jump to bracket" },
   { key: 'ctrl+t', category: 'Navigating Code', capital_key: "CTRL+T", command: 'show_all_symbols', description: "Show all symbols" },
   { key: 'ctrl+shift+o', category: 'Navigating Code', capital_key: "CTRL+SHIFT+O", command: 'go_to_symbol', description: "Go to symbol" },
-  { key: 'ctrl+shift+m', category: 'Debugging', capital_key: "CTRL+SHIFT+M", command: 'view_problems', description: "View problems" },
+  // { key: 'ctrl+shift+m', category: 'Debugging', capital_key: "CTRL+SHIFT+M", command: 'view_problems', description: "View problems" },
   { key: 'ctrl+shift+l', category: 'Multicursor', capital_key: "CTRL+SHIFT+L", command: 'cursor_at_all_occurrences', description: "Add a cursor at all occurrences" },
   { key: 'ctrl+shift+space', category: 'IntelliSense', capital_key: "CTRL+SHIFT+SPACE", command: 'trigger_parameter_hints', description: "Trigger parameter hints" },
   { key: 'ctrl+\\',          category: 'Using VSCode', capital_key: "CTRL+\\",          command: 'split_editor', description: "Split editor" },
@@ -14,9 +14,12 @@ export const KEY_MAP: { key: string; category: string; capital_key: string; comm
   { key: 'ctrl+g',     category: 'Navigating Code', capital_key: "CTRL+G",     command: 'go_to_line', description: "Go to line" }, // this is where I started adding new stuff
   { key: 'ctrl+.',     category: 'Navigating Code', capital_key: "CTRL+.",     command: 'quick_fix', description: "Quick Fix" },
   { key: 'ctrl+shift+s',     category: 'Using VSCode', capital_key: "CTRL+SHIFT+S",     command: 'save_file_as', description: "Save File As" },
-  { key: 'alt+up', category: 'Editing', capital_key: "ALT+UP", command: 'move_line_up', description: "Move line up" },
-  { key: 'alt+down', category: 'Editing', capital_key: "ALT+DOWN", command: 'move_line_down', description: "Move line down" },
-  { key: 'ctrl+l', category: 'Editing', capital_key: "CTRL+L", command: 'select_line', description: "Select line" },
+  { key: 'alt+up', category: 'Editing', capital_key: "ALT+UP", command: 'move_line_up', description: "Move line up" },   
+  { key: 'alt+down', category: 'Editing', capital_key: "ALT+DOWN", command: 'move_line_down', description: "Move line down" },   
+  { key: 'ctrl+l', category: 'Editing', capital_key: "CTRL+L", command: 'select_line', description: "Select line" },   
+  { key: 'shift+alt+i', category: 'Multicursor', capital_key: "SHIFT+ALT+I", command: 'insert_cursor_at_end_of_each_line_selected', description: "Insert cursor at end of each line selected" },
+  { key: 'ctrl+shift+up', category: 'Multicursor', capital_key: "CTRL+SHIFT+UP", command: 'add_cursor_above', description: "Add cursor above" },
+  { key: 'ctrl+shift+down', category: 'Multicursor', capital_key: "CTRL+SHIFT+DOWN", command: 'add_cursor_below', description: "Add cursor below" },
 ]; 
     
   
